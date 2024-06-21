41fd2ef02cdbdf0af23ce887a9632f94f005b592
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-06-21 14:25:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a new line.
  * Return: Always 0.
  */
