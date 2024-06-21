bc92bf3596092d3348b318a79f664c7ac83e274c
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-06-21 14:23:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,14 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
+ * main - prints "Programming is like building a multilingual 
+ * puzzle, followed by a new line.
  * Return: Always 0.
  */
 
 int main(void)
 
 {
-	puts("\"Programming is like building a multinlingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
