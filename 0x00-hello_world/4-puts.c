8a5939712d7f5bc54efa9010b0eed6848cad62e5
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-06-21 14:16:59 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle", followed by a new line.
+ * return 0.
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building an multinlingual puzzle");
+	return(0);
+}
