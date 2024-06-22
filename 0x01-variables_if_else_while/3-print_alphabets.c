31c3682f87f5706f40d913be67751da955305186
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-06-21 17:01:53 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	char alph;
 
-    for (alph = 'a'; alph <= 'z'; alph++)
-            putchar(alph);
-    for (alph = 'A'; alph <= 'Z'; alph++)
-            putchar(alph);
-    putchar('\n');
-    return (0);
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+	for (alph = 'A'; alph <= 'Z'; alph++)
+		putchar(alph);
+	putchar('\n');
+	return (0);
 }
