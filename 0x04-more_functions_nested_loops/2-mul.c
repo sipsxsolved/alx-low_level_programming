7daccc46ca1e36ab3192a41a1979f009ef42538b
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-06-27 20:19:33 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * Description: multiply two numbers
+ * @a: interger value
+ * @b: interger value
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result = 0;
+
+	result = a * b;
+	return (result);
+}
