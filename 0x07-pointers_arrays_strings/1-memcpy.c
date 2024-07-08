22cc863bfe16d42b22be7dc4c11eb84a1979e6ac
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-07-08 15:04:10 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+/**
+* _memcpy - function name
+* @dest: destination memory area
+* @src: source memory area
+* @n: number of bytes
+*
+* Description: a function that copies memory area
+* Return: pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
