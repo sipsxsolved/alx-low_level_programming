4e3a4dd17966c418fa667220e82260b81bd3b380
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-07-22 14:27:20 -0700

    add 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: a function that prints name of current file
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
