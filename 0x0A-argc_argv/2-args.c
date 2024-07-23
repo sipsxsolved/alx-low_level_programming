6c4ed3d4b5495082ef19430b0919acecf6e108e9
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-07-23 05:02:10 -0700

    add 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* @argc: argument count
+* @argv: argument vector
+*
+* Description: a program that prints all its args
+* Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
