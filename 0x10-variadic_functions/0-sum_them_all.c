befef54dceaf5cfbca1f6176009ebbf7496171e2
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-07-25 04:21:14 -0700

    add 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,14 +17,15 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (i = 0; i < n; i++)
+	if (n != 0)
 	{
-		arg = va_arg(args, int);
-		if (arg == 0)
-			return (0);
-		result += arg;
+		for (i = 0; i < n; i++)
+		{
+			arg = va_arg(args, int);
+			result += arg;
+		}
+		va_end(args);
 	}
-	va_end(args);
 
 	return (result);
 }
