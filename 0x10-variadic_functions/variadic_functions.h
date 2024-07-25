9cc5a543f4a6bfd324e260c54b982a18921aa0a3
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-07-25 04:41:32 -0700

    add 3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct format_types - Struct format_types
+ * @identifier: The conversion specifier
+ * @f: The function pointer
+ */
+typedef struct format_types
+{
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} f_dt;
+
 #endif
